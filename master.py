d795c8667661d59f6ec9e17dd5d1eb1213d4de68
Aref Hasan <arefhasan@mobile-238-002.wlan.uni-mannheim.de>
2023-12-13 14:20:53 +0100

    playground has been updated

--- master.py
+++ master.py
@@ -1,2 +1,12 @@
 from manim import *
-from manimlib import *
+
+
+class Main(Scene):
+    def construct(self):
+        self.greeting()
+
+    def greeting(self):
+        text = Text("Hallo Zusammen!", font_size= 50)
+        self.play(Write(text))
+        self.wait(2)  
+        self.clear()
