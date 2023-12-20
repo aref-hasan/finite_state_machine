a6941cc6c4d9da88094fd1465b1da9419af18c34
Nik Yakovlev <nikyakovlev@mobile-233-043.wlan.uni-mannheim.de>
2023-12-20 13:43:16 +0100

    playground update

--- playground/nik_sandbox.py
+++ playground/nik_sandbox.py
@@ -55,9 +55,17 @@ class FiniteStateMachine(Scene):
             MathTex(r"F \text{ ist die Menge der Endzustände, eine Teilmenge von } Q.").scale(0.5).next_to(state1_img, DOWN, buff=5.5)
         ]
 
+
+        # Display FSM Components
+        for text_obj in fsm_components_text:
+            self.play(Write(text_obj))
+            self.wait(2)
+
         # Text parts of the expression
         text1 = MathTex("Q = \\{").scale(0.5)
         text2 = MathTex("\\}").scale(0.5)
+        text3 = MathTex(",").scale(0.5)
+        text4 = MathTex(",").scale(0.5)
 
         # Image objects (replace 'image_path' with the path to your images)
         image1 = ImageMobject("/Users/nikyakovlev/finite_state_machine/src/img/pokemon/PNG/charmander.png").scale(0.2)
@@ -65,19 +73,69 @@ class FiniteStateMachine(Scene):
         image3 = ImageMobject("/Users/nikyakovlev/finite_state_machine/src/img/pokemon/PNG/charizard.png").scale(0.2)
 
         # Positioning the objects
-        text1.next_to(fsm_components_text[1], RIGHT, buff=1)
+        text1.next_to(fsm_components_text[1], RIGHT, buff=3)
         image1.next_to(text1, RIGHT, buff=0.1)
-        image2.next_to(image1, RIGHT, buff=0.1)
-        image3.next_to(image2, RIGHT, buff=0.1)
+        text3.next_to(image1, RIGHT, buff=0.1)
+        image2.next_to(text3, RIGHT, buff=0.1)
+        text4.next_to(image2, RIGHT, buff=0.1)
+        image3.next_to(text4, RIGHT, buff=0.1)
         text2.next_to(image3, RIGHT, buff=0.1)
 
         # Add objects to the scene
-        self.add(text1, image1, image2, image3, text2)
+        self.add(text1, image1, text3, image2, text4, image3, text2)
 
-        # Display FSM Components
-        for text_obj in fsm_components_text:
-            self.play(Write(text_obj))
-            self.wait(2)
+        # Text parts of the expression
+        text1 = MathTex("\Sigma = \\{").scale(0.5)
+        text2 = MathTex("\\}").scale(0.5)
+        text3 = MathTex(",").scale(0.5)
+        
+
+        # Image objects (replace 'image_path' with the path to your images)
+        text16 = Text("Lvl 16").scale(0.3)
+        text36 = Text("Lvl 36").scale(0.3)
+
+        # Positioning the objects
+        text1.next_to(fsm_components_text[2], RIGHT, buff=1.2)
+        text16.next_to(text1, RIGHT, buff=0.1)
+        text3.next_to(text16, RIGHT, buff=0.1)
+        text36.next_to(text3, RIGHT, buff=0.1)
+        text2.next_to(text36, RIGHT, buff=0.1)
+
+        # Add objects to the scene
+        self.add(text1, text16, text3, text36, text2)
+
+         # Text parts of the expression
+        text1 = MathTex("\delta :").scale(0.5)
+        text2 = MathTex(",").scale(0.5)
+        text3 = MathTex("+").scale(0.5)
+        text4 = MathTex(r"\rightarrow").scale(0.5)
+        text5 = MathTex("+").scale(0.5)
+        text6 = MathTex(r"\rightarrow").scale(0.5)
+
+        # Image objects (replace 'image_path' with the path to your images)
+        text16 = Text("Lvl 16").scale(0.3)
+        text36 = Text("Lvl 36").scale(0.3)
+        image1 = ImageMobject("/Users/nikyakovlev/finite_state_machine/src/img/pokemon/PNG/charmander.png").scale(0.2)
+        image2 = ImageMobject("/Users/nikyakovlev/finite_state_machine/src/img/pokemon/PNG/charmeleon.png").scale(0.2)
+        image2_2 = ImageMobject("/Users/nikyakovlev/finite_state_machine/src/img/pokemon/PNG/charmeleon.png").scale(0.2)
+        image3 = ImageMobject("/Users/nikyakovlev/finite_state_machine/src/img/pokemon/PNG/charizard.png").scale(0.2)
+
+        # Positioning the objects
+        text1.next_to(fsm_components_text[3], RIGHT, buff=1.2)
+        image1.next_to(text1, RIGHT, buff=0.1)
+        text3.next_to(image1, RIGHT, buff=0.1)
+        text16.next_to(text3, RIGHT, buff=0.1)
+        text4.next_to(text16, RIGHT, buff=0.1)
+        image2.next_to(text4, RIGHT, buff=0.1)
+        text2.next_to(image2, RIGHT, buff=0.1)
+        image2_2.next_to(text2, RIGHT, buff=0.1)
+        text5.next_to(image2_2, RIGHT, buff=0.1)
+        text36.next_to(text5, RIGHT, buff=0.1)
+        text6.next_to(text36, RIGHT, buff=0.1)
+        image3.next_to(text6, RIGHT, buff=0.1)
+
+        # Add objects to the scene
+        self.add(text1, image1, text3, text16, text4, image2, text2, image2_2, text5, text36, text6, image3)
 
         # Keep the scene displayed
         self.wait(2)
