27cb008b1bd34ffab6953ed222fa94197fc9f5e2
arefhasan <arefhasanx@gmail.com>
2024-01-10 17:21:12 +0100

    NEA vs. DEA

--- project/master.py
+++ project/master.py
@@ -5,31 +5,33 @@ from manim_voiceover import VoiceoverScene
 from manim.scene.moving_camera_scene import MovingCameraScene
 from manim_voiceover.services.gtts import GTTSService
 from manim_voiceover.services.recorder import RecorderService
-###########################################################################
-###########################################################################
-###########################################################################
+
+###########################################################################################################
+################################################MAIN#######################################################
+###########################################################################################################
 
 class Main(MovingCameraScene):
     def construct(self):
-        self.greeting()
-        self.welcome()
-        self.pokemon_logo()
-        self.vid()
-        #self.sound()
-        self.pokemon_fig()
-        self.pokemon_components()
-        self.pokemon_components_math()
+        #self.greeting()
+        #self.welcome()
+        #self.pokemon_logo()
+        #self.vid()
+        #self.pokemon_fig()
+        #self.pokemon_components()
+        #self.pokemon_components_math()
+        self.dea()
         self.dea_name()
         self.nea_title()
         self.nea_title_short()
         self.question_mark_animation()
         self.show_description()
         self.nea_title_short_up()
+        self.nea_graph()
 
     def greeting(self):
         text = Text("Hallo Zusammen!", font_size= 50)
         self.play(Write(text))
-        self.wait(2)  
+        self.wait(3)  
         self.clear()
 
     def welcome(self):
@@ -46,7 +48,7 @@ class Main(MovingCameraScene):
         self.image_logo.move_to(ORIGIN)
 
         self.play(FadeIn(self.image_logo))
-        self.wait(2)
+        self.wait(4)
         
         #self.play(ShrinkToCenter(self.image_logo))
     def vid(self):
@@ -127,7 +129,7 @@ class Main(MovingCameraScene):
         text3 = MathTex(",").scale(0.5)
         text4 = MathTex(",").scale(0.5)
 
-        # Image objects (replace 'image_path' with the path to your images)
+        # Image 
         image1 = ImageMobject("../src/img/pokemon/PNG/charmander.png").scale(0.2)
         image2 = ImageMobject("../src/img/pokemon/PNG/charmeleon.png").scale(0.2)
         image3 = ImageMobject("../src/img/pokemon/PNG/charizard.png").scale(0.2)
@@ -152,7 +154,7 @@ class Main(MovingCameraScene):
         text2 = MathTex("\\}").scale(0.5)
         text3 = MathTex(",").scale(0.5)
         
-        # Image objects (replace 'image_path' with the path to your images)
+        # Image 
         text16 = Text("Lvl 16").scale(0.3)
         text36 = Text("Lvl 36").scale(0.3)
 
@@ -176,7 +178,7 @@ class Main(MovingCameraScene):
         text5 = MathTex("+").scale(0.5)
         text6 = MathTex(r"\rightarrow").scale(0.5)
 
-        # Image objects (replace 'image_path' with the path to your images)
+        # Image 
         text16 = Text("Lvl 16").scale(0.3)
         text36 = Text("Lvl 36").scale(0.3)
         image1 = ImageMobject("../src/img/pokemon/PNG/charmander.png").scale(0.2)
@@ -206,7 +208,7 @@ class Main(MovingCameraScene):
         text1 = MathTex(r"q_0 = ").scale(0.5)
         text2 = MathTex(r"\in Q").scale(0.5)
 
-        # Image objects (replace 'image_path' with the path to your images)
+        # Image 
         image1 = ImageMobject("../src/img/pokemon/PNG/charmander.png").scale(0.2)
 
         # Positioning the objects
@@ -223,7 +225,8 @@ class Main(MovingCameraScene):
         # Text parts of the expression
         text1 = MathTex(r"F = \{").scale(0.5)
         text2 = MathTex(r"\} \subseteq Q").scale(0.5)
-        # Image objects (replace 'image_path' with the path to your images)
+
+        # Image
         image1 = ImageMobject("../src/img/pokemon/PNG/charizard.png").scale(0.2)
 
         # Positioning the objects
@@ -241,8 +244,85 @@ class Main(MovingCameraScene):
         self.clear()
         
 ###########################DEA###########################
+    def dea(self):
+        # The 3 begining circles
+        circle2 = Circle(radius=0.6, color=BLUE)
+        circle2.move_to(ORIGIN)
+        circle1 = Circle(radius=0.6, color=BLUE).next_to(circle2, LEFT, buff=2)
+        circle3 = Circle(radius=0.6, color=BLUE).next_to(circle2, RIGHT, buff=2)
         
+        # text inside the circles
+        text1 = Text("1", font_size=24).move_to(circle1.get_center())
+        text2 = Text("2", font_size=24).move_to(circle2.get_center())
+        text3 = Text("3", font_size=24).move_to(circle3.get_center())
 
+        # straight arrows
+        arrow1 = Arrow(start=circle1.get_top() + DOWN * 0.4, end=circle2.get_top() + DOWN * 0.4, buff=0.7) 
+        arrow2 = Arrow(start=circle2.get_top() + DOWN * 0.4, end=circle3.get_top() + DOWN * 0.4, buff=0.7)
+        arrow3 = Arrow(start=circle2.get_bottom() + UP * 0.4, end=circle1.get_bottom() + UP * 0.4, buff=0.7)
+
+        # straight arrow labels
+        label1 = Text("a").next_to(arrow1, UP, buff=0.1).scale(0.5)
+        label2 = Text("b").next_to(arrow2, UP, buff=0.1).scale(0.5)
+        label3 = Text("a").next_to(arrow3, DOWN, buff=0.1).scale(0.5)
+
+        # double circle for circle 3
+        circle_around_state3 = Circle(color=BLUE).scale(0.5)
+        circle_around_state3.move_to(circle3.get_center())
+
+        # arrow for iniial state
+        start_point = circle1.get_center() + LEFT * 2 + UP * 1 
+        diagonal_arrow = Arrow(start=start_point, end=circle1.get_left(), buff=0.1)
+
+        # coordinates for curved arrows
+        radius = circle1.width / 1.5
+        angle = 52 * DEGREES  
+        start_x = radius * np.cos(angle)/ 1.6
+        start_y = radius * np.sin(angle) 
+        start_point_1 = circle1.get_center() + np.array([start_x, start_y, 5])
+        start_point_3 = circle3.get_center() + np.array([start_x, start_y, 5])
+        end_angle = angle + 45.5
+        end_x = radius * np.cos(end_angle) / 1.6 
+        end_y = radius * np.sin(end_angle)
+        end_point_1 = circle1.get_center() + np.array([end_x, end_y, 2])
+        end_point_3 = circle3.get_center() + np.array([end_x, end_y, 2])
+
+        # curved arrows
+        arrow_1 = CurvedArrow(start_point_1, end_point_1, color=WHITE, angle= 3)
+        arrow_3 = CurvedArrow(start_point_3, end_point_3, color=WHITE, angle= 3)
+
+        # curved arrow labels
+        label_1 = Text("b", font_size=24, color=WHITE)
+        label_3 = Text("a, b", font_size=24, color=WHITE)
+        label_offset = np.array([0.2, 0.2, 0])
+        label_1.move_to(arrow_1.point_from_proportion(0.5) + label_offset)
+        label_3.move_to(arrow_3.point_from_proportion(0.5) + label_offset)
+
+
+        self.dea_all_elements = Group(diagonal_arrow, circle1, text1, arrow_1, label_1, arrow1, label1, arrow3, label3, circle2, text2, arrow2, label2, circle3, text3, arrow_3, label_3, circle_around_state3)
+
+
+        self.play(Create(diagonal_arrow))
+        self.wait(3)
+        self.play(FadeIn(circle1), Write(text1))
+        self.wait(3)
+        self.play(Create(arrow_1), Write(label_1)) 
+        self.wait(3)
+        self.play(Create(arrow1), Write(label1))
+        self.wait(3)
+        self.play(Create(arrow3), Write(label3))
+        self.wait(3)
+        self.play(FadeIn(circle2), Write(text2))  
+        self.wait(3)
+        self.play(Create(arrow2), Write(label2))
+        self.wait(3)
+        self.play(FadeIn(circle3), Write(text3))
+        self.wait(3)
+        self.play(Create(arrow_3), Write(label_3))  
+        self.wait(3)
+        self.play(Create(circle_around_state3))
+
+        self.clear()
 
 ###########################NEA###########################
     def dea_name(self):
@@ -282,10 +362,110 @@ class Main(MovingCameraScene):
         self.play(self.nea.animate.to_edge(UP))  # Move the title to left edge.
         self.wait(2)
 
-
-    def sound(self):
-        self.play(Write(Text("Hello World!")), run_time=3)
+    def nea_graph(self):
+        # The 3 begining circles
+        circle2 = Circle(radius=0.6, color=BLUE)
+        circle2.move_to(ORIGIN)
+        circle1 = Circle(radius=0.6, color=BLUE).next_to(circle2, LEFT, buff=2)
+        circle3 = Circle(radius=0.6, color=BLUE).next_to(circle2, RIGHT, buff=2)
         
-        self.add_sound("../src/sounds/pokemon_original.mp3")
+        # text inside the circles
+        text1 = Text("1", font_size=24).move_to(circle1.get_center())
+        text2 = Text("2", font_size=24).move_to(circle2.get_center())
+        text3 = Text("3", font_size=24).move_to(circle3.get_center())
 
-        self.play(FadeOut(Text("Hello World!")), run_time=1)
+        # straight arrows
+        arrow1 = Arrow(start=circle1.get_top() + DOWN * 0.4, end=circle2.get_top() + DOWN * 0.4, buff=0.7) 
+        arrow2 = Arrow(start=circle2.get_top() + DOWN * 0.4, end=circle3.get_top() + DOWN * 0.4, buff=0.7)
+        arrow3 = Arrow(start=circle2.get_bottom() + UP * 0.4, end=circle1.get_bottom() + UP * 0.4, buff=0.7)
+
+        # straight arrow labels
+        label1 = Text("a").next_to(arrow1, UP, buff=0.1).scale(0.5)
+        label2 = Text("b").next_to(arrow2, UP, buff=0.1).scale(0.5)
+        label3 = Text("a").next_to(arrow3, DOWN, buff=0.1).scale(0.5)
+
+        # double circle for circle 3
+        circle_around_state3 = Circle(color=BLUE).scale(0.5)
+        circle_around_state3.move_to(circle3.get_center())
+
+        # arrow for iniial state
+        start_point = circle1.get_center() + LEFT * 2 + UP * 1 
+        diagonal_arrow = Arrow(start=start_point, end=circle1.get_left(), buff=0.1)
+
+        # coordinates for curved arrows
+        radius = circle1.width / 1.5
+        angle = 52 * DEGREES  
+        start_x = radius * np.cos(angle)/ 1.6
+        start_y = radius * np.sin(angle) 
+        start_point_1 = circle1.get_center() + np.array([start_x, start_y, 5])
+        start_point_3 = circle3.get_center() + np.array([start_x, start_y, 5])
+        end_angle = angle + 45.5
+        end_x = radius * np.cos(end_angle) / 1.6 
+        end_y = radius * np.sin(end_angle)
+        end_point_1 = circle1.get_center() + np.array([end_x, end_y, 2])
+        end_point_3 = circle3.get_center() + np.array([end_x, end_y, 2])
+
+        # curved arrows
+        arrow_1 = CurvedArrow(start_point_1, end_point_1, color=WHITE, angle= 3)
+        arrow_3 = CurvedArrow(start_point_3, end_point_3, color=WHITE, angle= 3)
+
+        # curved arrow labels
+        label_1 = Text("a, b", font_size=24, color=WHITE)
+        label_3 = Text("a, b", font_size=24, color=WHITE)
+        label_offset = np.array([0.2, 0.2, 0])
+        label_1.move_to(arrow_1.point_from_proportion(0.5) + label_offset)
+        label_3.move_to(arrow_3.point_from_proportion(0.5) + label_offset)
+        
+        # red rectangle
+        surrounding_rectangle = SurroundingRectangle(label_1, buff=.1, color="red")
+        divider_line = Line(UP*2, DOWN*2)
+
+
+
+        all_elements = Group(diagonal_arrow, circle1, text1, arrow_1, label_1, arrow1,
+                            label1, arrow3, label3, circle2, text2, arrow2, label2,
+                            circle3, text3, arrow_3, label_3, circle_around_state3,surrounding_rectangle)
+
+
+        self.play(Create(diagonal_arrow))
+        self.wait(3)
+        self.play(FadeIn(circle1), Write(text1))
+        self.wait(3)
+        self.play(Create(arrow_1), Write(label_1)) 
+        self.wait(3)
+        self.play(Create(arrow1), Write(label1))
+        self.wait(3)
+        self.play(Create(arrow3), Write(label3))
+        self.wait(3)
+        self.play(FadeIn(circle2), Write(text2))  
+        self.wait(3)
+        self.play(Create(arrow2), Write(label2))
+        self.wait(3)
+        self.play(FadeIn(circle3), Write(text3))
+        self.wait(3)
+        self.play(Create(arrow_3), Write(label_3))  
+        self.wait(3)
+        self.play(Create(circle_around_state3))
+        self.wait(5)
+        #red rectangle
+        self.play(Create(surrounding_rectangle))  
+        self.wait(2)
+
+        # move nea - right
+        self.play(all_elements.animate.scale(0.6).to_edge(RIGHT, buff=0.5))
+
+        # move title to graph
+        self.play(self.nea.animate.scale(0.7).to_edge(RIGHT, buff=2.8))
+ 
+        # DEA graph
+        self.dea_all_elements.scale(0.6).to_edge(LEFT, buff=0.5)
+        self.add(Text("DEA", font_size= 50).scale(0.7).to_edge(UP).to_edge(LEFT, buff=2.8))
+        self.wait(2)
+        self.add(self.dea_all_elements)
+        self.wait(2)  
+
+        # a line as divider 
+        self.play(Create(divider_line))
+
+        self.clear()
+        
